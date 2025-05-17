d6df57c94b5df86a28842d0b878e6b5a21b7c806
nirobsarker012 <nirobsarker012@gmail.com>
2025-05-17 15:52:21 +0600

    added: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../assets/images/more/logo1.png";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <nav className="bg-[url('assets/images/more/15.jpg')] bg-cover bg-center">
+          <NavLink to={"/"} className="flex justify-center items-center gap-4">
+            <img className="w-[75px] h-[90px]" src={logo} alt="" />
+            <h2 className="text-white text-[60px]">Expresso Emporium</h2>
+          </NavLink>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../layout/Root";
 import Home from "../pages/Home";
+import AddCoffee from "../components/AddCoffee";
+import UpdateCoffe from "../components/UpdateCoffe";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "addCoffe",
+        Component: AddCoffee,
+      },
+      {
+        path: "updateCoffee",
+        Component: UpdateCoffe,
       },
     ],
   },
